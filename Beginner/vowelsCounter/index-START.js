86dc655830468c9a43f57b13e94dd422ab356bde
Thomas Carney <thomascarney315@gmail.com>
2023-08-22 18:32:36 -0400

    vowelsCounter algos complete

--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -4,10 +4,25 @@ e.g vowelsCounter('anehizxcv') // will return 3
 */
 
 
+// function vowelsCounter(text) {
+//     let vowels = 'aeiou'.split('')
+//     return text.toLowerCase().split('').filter(char => vowels.includes(char)).length
+// }
+
+// function vowelsCounter(text) {
+//     let vowels = ['a', 'e', 'i', 'o', 'u']
+//     let counter = 0
+//     for (let char of text.toLowerCase()) {
+//         if (vowels.includes(char)){
+//             counter++
+//         }
+//     }
+//     return counter
+// }
+
 function vowelsCounter(text) {
-    // Code goes here
+    const matches = text.match(/[aeiou]/gi)
+    return matches.length
 }
-
-
 
 module.exports = vowelsCounter;
