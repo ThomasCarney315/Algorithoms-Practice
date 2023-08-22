79a4c352ed25c74c2cbbc63169ed8349b6bea91d
Thomas Carney <thomascarney315@gmail.com>
2023-08-22 19:14:41 -0400

    maxRecuuringChar algos complete

--- Beginner/maxRecurringChar/index-START.js
+++ Beginner/maxRecurringChar/index-START.js
@@ -6,7 +6,34 @@ e.g maxRecurringChar('aabacada') // will return 'a'
 
 
 function maxRecurringChar(text) {
-    // Code goes here
+    let charMap = {}
+    let maxValue = 0
+    let maxChar = ''
+    // let charArray = []
+    // let valuesArray = []
+
+    for (let char of text) {
+        if (charMap.hasOwnProperty(char)) { //alternative if statement (char in charMap)
+            charMap[char] ++
+        }
+        else {
+            charMap[char] = 1
+        }
+    }
+
+    for (let char in charMap) {
+        if (charMap[char] > maxValue) {
+            maxValue = charMap[char]
+            maxChar = char
+        }
+    }
+
+    // charArray = Object.keys(charMap)
+    // valuesArray = Object.values(charMap)
+    // maxValue = Math.max(...valuesArray)
+    // maxChar = charArray[valuesArray.indexOf(maxValue)]
+    
+    return maxChar
 }
 
 
