d2dd46d899662956e1ccc6b11ccb9f18ca09e3be
Thomas Carney <thomascarney315@gmail.com>
2023-08-23 22:29:17 -0400

    palindromeChecker algos complete

--- Beginner/palindromeChecker/index-START.js
+++ Beginner/palindromeChecker/index-START.js
@@ -6,10 +6,28 @@ e.g palindromeChecker('racecar') // will return true
 
 
 
-function palindromeChecker(text) {
-    // Code goes here
-}
+// function palindromeChecker(text) {
+//     text.toLowerCase()
+//     return text === text.split('').reverse().join('')
+// }
 
+// function palindromeChecker(text) {
+//     let charArr = text.toLowerCase().split('')
+
+//     return charArr.every((char, idx) => {
+//         return char === charArr[charArr.length - idx - 1]
+//     })
+// }
+
+//this method only interates half of the string to safe time
+function palindromeChecker(text) {
+    let len = text.length
+    for (let i = 0; i < len / 2; i++) {
+        if (text[i] !== text[len - i - 1])
+            return false
+    }
+    return true
+}
 
 
 module.exports = palindromeChecker;
