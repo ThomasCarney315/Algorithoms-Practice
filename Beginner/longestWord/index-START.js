c77a9bca6a456720ed306f8587e898a2c3c2c971
Thomas Carney <thomascarney315@gmail.com>
2023-09-04 17:27:10 -0400

    longestWord algos complete

--- Beginner/longestWord/index-START.js
+++ Beginner/longestWord/index-START.js
@@ -6,8 +6,33 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 
 
 function longestWord(text) {
-    // Code goes here
+    let words = text.split(' ')
+    let longestWordIndex = 0 
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length > words[longestWordIndex].length``) {
+            longestWordIndex = i
+        }
+    }
+    return words[longestWordIndex]
 }
 
+// function longestWord(text) {
+//     let words = text.split(' ')
+//     let lengths = words.map(word => word.length)
+//     let indexOfMax = lengths.indexOf(Math.max(...lengths))
+
+//     return words[indexOfMax]
+// }
+
+// function longestWord(text) {
+//     return text.split(' ').reduce((longest, current) => current.length > longest.length ? current : longest) 
+    
+// }
+
+// function longestWord(text) {
+//     let words = text.split(' ')
+//     words.sort((a,b) => b.length - a.length)
+//     return words[0]
+// }
 
 module.exports = longestWord
