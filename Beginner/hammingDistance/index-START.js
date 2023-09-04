f69dfbb0b2203a365f8945ac2289cdb7b3011fe3
Thomas Carney <thomascarney315@gmail.com>
2023-09-04 15:39:17 -0400

    hammingDistance algo complete

--- Beginner/hammingDistance/index-START.js
+++ Beginner/hammingDistance/index-START.js
@@ -6,10 +6,40 @@ E.g hammingDistance('rover', 'river') // should return 1
 
 
 
-function hammingDistance(stringA, stringB) {
-    // Code goes here
-}
+// function hammingDistance(stringA, stringB) {
+//     let count = 0
+//     if (stringA.length !== stringB.length) {
+//         throw new Error('Strings do not have equal length')
+//     }
+//     else {
+//         stringA.toLowerCase()
+//         stringB.toLowerCase()
+//         for (let i = 0; i < stringA.length; i++) {
+//             if (stringA[i] != stringB[i]) {
+//                 count++
+//         }
+//             }
+//     }
+//     return count
+// }
 
+function hammingDistance(stringA, stringB) {
+    let result = 0
+
+    if (stringA.length == stringB.length) {
+
+        for (let i = 0; i < stringA.length; i++) {
+            if (stringA[i].toLowerCase() != stringB[i].toLowerCase()) {
+                result++
+            }
+        }
+
+        return result
+
+    } else {
+        throw new Error('Strings do not have equal length')
+    }
+}
 
 
 module.exports = hammingDistance
