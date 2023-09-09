a50dc3499842d26d4a0a7acff9e2bf53bb7ae056
Thomas Carney <thomascarney315@gmail.com>
2023-09-08 21:09:25 -0400

    chunkArray algos complete

--- Beginner/chunkArray/index-START.js
+++ Beginner/chunkArray/index-START.js
@@ -4,9 +4,42 @@
  E.g  mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
+// function chunkArray(array, size) {
+//     let chunks = []
+
+//     for (let i = 0; i < array.length; i += size) {
+//         chunks.push(array.slice(i, i + size))
+//     }
+
+//     return chunks
+// }
+
+// function chunkArray(array, size) {
+//     let result = []
+//     for (value of array){
+//         let lastArray = result[result.length -1 ]
+//         if(!lastArray || lastArray.length == size){
+//             result.push([value])
+//         } else{
+//             lastArray.push(value)
+//         }
+//     }
+//     return result
+// }
+
+// function chunkArray(array, size) {
+//     let result = []
+//     let arrayCopy = [...array]
+//     while (arrayCopy.length > 0) {
+//         result.push(arrayCopy.splice(0, size))
+//     }
+//     return result
+// }
+
 function chunkArray(array, size) {
-    // Code goes here
-}
-
-
+    if(array.length <= size){
+        return [array]
+    }
+    return [array.slice(0,size), ...chunkArray(array.slice(size), size)]
+ }
 module.exports = chunkArray
